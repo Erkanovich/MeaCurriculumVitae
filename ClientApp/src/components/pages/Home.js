24161b920b64707c2bf3cf1e7eae52151df37570
Erkanovich <erinik90@gmail.com>
2021-05-20 00:13:00 +0200

    inital commit

---
+++ ClientApp/src/components/pages/Home.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Promise } from 'q';
+import './Home.css';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      typeWriterText: ''
+    }
+  }
+
+  componentDidMount() {
+    this.typeWriter();
+  }
+
+  async typeWriter() {
+    let textsToType = ['developer', 'teacher', 'philosopher', 'crypto enthusiast.'];
+    for (let i = 0; i < textsToType.length; i++) {
+      await this.typeText(textsToType[i]);
+      if (i !== textsToType.length -1) {
+        await this.eraseText(textsToType[i]);
+      }
+    }
+  }
+
+  async typeText(someText) {
+    for (let i = 0; i < someText.length; i++) {
+      await this.typeChar(someText[i]);
+    }
+  }
+
+  async eraseText(someText) {
+    for (let i = someText.length; i > 0; i--) {
+        await this.eraseChar();
+    }
+  }
+
+  typeChar(someChar) {
+    return new Promise(resolve => {
+      let updatedText = this.state.typeWriterText + someChar
+      this.setState({
+        typeWriterText: updatedText
+      });
+      setTimeout(resolve, Math.random() * 200)
+    });
+  }
+
+  eraseChar() {
+    return new Promise(resolve => {
+      this.setState({
+        typeWriterText: this.state.typeWriterText.slice(0,-1)
+      });
+      setTimeout(resolve, 20)
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>I'm Erik Niklasson</h1>
+        <h1 className="typewriter">I'm a
+          <span> {this.state.typeWriterText}</span><span className="blink_me">|</span>
+        </h1>
+      </div>
+    );
+  }
+}
